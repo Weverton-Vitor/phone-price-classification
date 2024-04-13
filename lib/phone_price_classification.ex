cba1074cc7e430fc77be489f51742b52830b2965
Weverton Vitor <wevertonvitor01@gmail.com>
2024-04-13 08:49:49 -0300

    first commit

---
+++ lib/phone_price_classification.ex
@@ -0,0 +1,18 @@
+defmodule PhonePriceClassification do
+  @moduledoc """
+  Documentation for `PhonePriceClassification`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhonePriceClassification.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phone_price_classification_test.exs
@@ -0,0 +1,8 @@
+defmodule PhonePriceClassificationTest do
+  use ExUnit.Case
+  doctest PhonePriceClassification
+
+  test "greets the world" do
+    assert PhonePriceClassification.hello() == :world
+  end
+end
